1a8ade68df0477826d5eb1a296ac94c0e492925e
jahmed-wq <108134666+jahmed-wq@users.noreply.github.com>
2023-04-03 23:46:02 +0530

    update-only-projectname

---
+++ vminstance/main.tf
@@ -0,0 +1,30 @@
+# This is the provider used to spin up the gcloud instance
+provider "google" {
+ 
+  project = "probable-maker-381908"
+  credentials = file("terraform.json")
+  region  = "us-east1"
+  
+}
+
+resource "google_compute_instance" "vm-instance" {
+  name         = "test"
+  machine_type = "f1-micro"
+  zone         = "us-east1-b"
+
+
+  boot_disk {
+    initialize_params {
+      image = "debian-10-buster-v20221102"
+    }
+  }
+    
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
+
